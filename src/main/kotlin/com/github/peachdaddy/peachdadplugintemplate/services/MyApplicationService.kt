aa4321ea9bdb03829055b214a88c6bf4dde681bc
GitHub Action <action@github.com>
2021-08-02 15:22:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/peachdaddy/peachdadplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.peachdaddy.peachdadplugintemplate.services
+
+import com.github.peachdaddy.peachdadplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/peachdaddy/peachdadplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/peachdaddy/peachdadplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.peachdaddy.peachdadplugintemplate.services
 
+import com.github.peachdaddy.peachdadplugintemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
